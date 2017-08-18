899045e449a7ee6a89668c2ac5f202343e0747ce
David Onder <donder@haywood.edu>
2017-08-18 08:37:08 -0400

    Updated to use code from importData (export and meta).

--- source/importData_wStatus.py
+++ source/importData_wStatus.py
@@ -7,14 +7,19 @@ import pandas as pd
 import numpy as np
 from os import path
 import regex
-import glob
 from datetime import date, timedelta
+import sqlalchemy
+from sqlalchemy import exc
+import datetime
+import meta
+import export
 
 with open("config.yml","r") as ymlfile:
     cfg = yaml.load(ymlfile)
 
 export_path = cfg['informer']['export_path_wStatus']
-output_path = cfg['informer']['output_path_wStatus']
+invalid_path = cfg['informer']['invalid_path_wStatus']
+archive_path = cfg['informer']['archive_path_wStatus']
 prefix = cfg['informer']['prefix']
 
 pattern = r'{0}(?P<fnpat>.*)___.*|(?P<fnpat>.*)___.*'.format(prefix)
@@ -38,6 +43,13 @@ for key in status_fields.keys():
     fields = status_fields[key]
     status_datetime_fields[key] = [ f for f in fields if date_regex.match(f) ]
 
+log = open("log/log_{0}.txt".format( datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S%f") ), "w")
+
+engine = export.engine(cfg['sql']['driver'], 
+                       cfg['sql']['server'],
+                       cfg['sql']['db'],
+                       cfg['sql']['schema'])
+
 # This will expand a dataframe by splitting a MV column into individual rows 
 def explode(df, cols, split_on=','):
     """
@@ -57,29 +69,34 @@ def explode(df, cols, split_on=','):
     df_explode.columns = cols
     return pd.concat((df_repeat, df_explode), axis=1)
 
-# Return the key(s) for the specified fle
-def getKeyFields(file=''):
-    global lookuplist
+def processfile(df, fn, d):
+    #columnArray = np.asarray(list(df.columns.values))
+    #df.reset_index(inplace=True)
+    columnHeaders = list(df.columns.values)
+    columnArray = np.asarray(columnHeaders)
+    kList, dTypes = meta.getDataTypes()
+    dTyper = {k: dTypes[k] for k in dTypes.keys() & columnArray}
+    kLister = {k: kList[k] for k in kList.keys() & columnArray}
+    for k, v in list(kLister.items()):
+        if v != 'K':
+            del kLister[k]
 
-    # If lookuplist exists, move along. Otherwise, create it
+    #log.write("dTyper: %s\n" % (dTyper))
+    #log.write("kLister: %s\n" % (kLister))
+
     try:
-        if lookuplist.empty:
-            pass
+        print("Updating fn = "+fn+", d = "+d)
+        export.executeSQL_UPDATE(engine, df, fn, dTyper, kLister, log)
+        # Define and create the directory for all the output files
+        # directory = '{path}/{folder}'.format(path=invalid_path,folder=fn)
+        # os.makedirs(directory,exist_ok=True)
+        # df.to_csv('{path}/{fn}_{dt}.csv'.format(path=directory,fn=fn,dt=d), index = False)
 
-    except NameError:
-        # Read all files in the meta folder
-        meta_path = cfg['informer']['export_path_meta_cdd']
-        all_files = glob.glob(os.path.join(meta_path, "*_CDD*csv"))
-        df_from_each_file = (pd.read_csv(f,encoding = "ISO-8859-1",dtype='str') for f in all_files)
-        lookuplist = pd.concat(df_from_each_file, ignore_index=True)
-        lookuplist = lookuplist.replace(np.nan, '', regex=True)
+    except:
+        print('\t ---Error in file: %s the folder will be skipped' % file)
+        raise
 
-    if file=='':
-        fl = lookuplist[(lookuplist['Database Usage Type ']=='K')]
-    else:
-        fl = lookuplist[(lookuplist['Source ']==file) & (lookuplist['Database Usage Type ']=='K')]
-
-    return(fl.ids.tolist())
+    return
 
 # Cycle through all the files in the folder
 for root, subdirs, files in os.walk(export_path):
@@ -99,7 +116,7 @@ for root, subdirs, files in os.walk(export_path):
             print("Processing "+fn+"...")
 
             df = pd.read_csv(csvinput,dtype='str')
-            file_keys = getKeyFields(fn.replace('_','.'))
+            file_keys = meta.getKeyFields(fn.replace('_','.'))
 
             # Fill down the status fields so all status fields have a value            
             for fld in df_status:
@@ -116,30 +133,39 @@ for root, subdirs, files in os.walk(export_path):
             else:
                 newDataDatetime = df[df_status_datetime[0]] + "T00:00:00"
 
-            df['DataDatetime']=newDataDatetime
+            df['DataDatetime']=pd.to_datetime(newDataDatetime)
 
             # Keep the latest record of any duplicated rows by all colums except the status fields
-            # TODO: Should keep date/time fields in the dup check
-            df = df.drop_duplicates(df.columns.difference(df_status_only),keep='last')
+            df = df.drop_duplicates(set(df.columns).symmetric_difference(set(df_status_only)),keep='last')
 
             # Set the index to the keys of the file for the groupby/splitting below
-            df = df.set_index(file_keys)
+            #df = df.set_index(file_keys)
 
-            # Define and create the directory for all the output files
-            directory = '{path}/{folder}'.format(path=output_path,folder=fn)
-            os.makedirs(directory,exist_ok=True)
+            # Make datetime variable an actual datetime instead of a string, then sort
+            df.sort_values(by='DataDatetime')
 
             # Define and create the directory for the INVALID output file
-            invalid_directory = '{path}/INVALID'.format(path=output_path)
-            os.makedirs(invalid_directory,exist_ok=True)
+            os.makedirs(invalid_path,exist_ok=True)
 
             # Remove from the dataframe all rows with an invalid date
-            df[df_status_datetime[0]] = pd.to_datetime(df[df_status_datetime[0]])
-            df[df[df_status_datetime[0]]>invalid_date].to_csv('{path}/{fn}_INVALID.csv'.format(path=invalid_directory,fn=fn))
-            df = df[df[df_status_datetime[0]]<=invalid_date]
+            #df[df_status_datetime[0]] = pd.to_datetime(df[df_status_datetime[0]])
+            df[pd.to_datetime(df[df_status_datetime[0]])>invalid_date].to_csv('{path}/{fn}_INVALID.csv'.format(path=invalid_path,fn=fn))
+            df = df[pd.to_datetime(df[df_status_datetime[0]])<=invalid_date]
 
             # Now, group by the date field and create cumulative files for each date in the file
-            for d in df[df_status_datetime[0]].dt.strftime('%Y-%m-%d').unique():
-                df.loc[df[df_status_datetime[0]] <= d] \
-                    .groupby(file_keys,as_index=False).last() \
-                    .to_csv('{path}/{fn}_{dt}.csv'.format(path=directory,fn=fn,dt=d))
+            try:
+                for d in sorted(df['DataDatetime'].dt.strftime('%Y-%m-%d').unique()):
+                    processfile(df.loc[df[df_status_datetime[0]] == d].groupby(file_keys,as_index=False).last(), fn, d)
+
+                # for key, data in df.groupby(df[df_status_datetime[0]],as_index=False):
+                # for key, data in df.groupby(df['DataDatetime'],as_index=False):
+                #     processfile(data, fn, key)
+                #for d in sorted(df['DataDatetime'].dt.strftime('%Y-%m-%d').unique()):
+                #    processfile(df.loc[df[df_status_datetime[0]] <= d].groupby(file_keys,as_index=False).last(),
+                    #processfile(df.loc[df[df_status_datetime[0]] <= d].groupby(file_keys,as_index=False).last(), fn, d)
+            except:
+                print('\t ---Error in file: %s' % fn)
+
+        print(".....archiving file "+file)
+        export.archive(fn,file,export_path,archive_path)
+
