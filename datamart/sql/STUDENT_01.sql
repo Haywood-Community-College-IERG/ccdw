aa98edeb58ebcd02eb32c8461e546488ab570804
David Onder <donder@haywood.edu>
2018-03-15 14:55:08 +0000

    Updated STUDENT_01.sql

--- datamart/sql/STUDENT_01.sql
+++ datamart/sql/STUDENT_01.sql
@@ -1,59 +1,40 @@
-IF OBJECT_ID('datamart.getSTUDENT_01', 'P') IS NOT NULL
+IF OBJECT_ID('datamart.getSTUDENT_01', 'IF') IS NOT NULL
 	DROP FUNCTION datamart.getSTUDENT_01
 GO
 
-CREATE FUNCTION datamart.getSTUDENT_01(@StudentID int)
-RETURNS @STUDENT_TERMS TABLE
-(	
-	[STTR.STUDENT] varchar(10) PRIMARY KEY NOT NULL,
-	[STTR.TERM] varchar(7) NOT NULL
+CREATE FUNCTION datamart.getSTUDENT_01(
+    @data_year varchar(4),
+	@data_term varchar(2),
+	@report_date date
 )
+RETURNS TABLE
 AS
-
-BEGIN
-	DECLARE
-		@STTRTERM varchar(7),
-		@STTRSTUDENT varchar(10),
-		@data_year varchar(4),
-		@data_term varchar(2),
-		@report_date date,
-		@term_id_ce varchar(20),
-		@term_id_cu varchar(20)
-
-	IF @data_term = '01'
-		BEGIN
-			SET @term_id_ce = @data_year + 'CE1'
-			SET @term_id_cu = @data_year + 'SP'
-		END;
-	ELSE BEGIN
-		IF @data_term = '02'
-			BEGIN
-				SET @term_id_ce = @data_year + 'CE2'
-				SET @term_id_cu = @data_year + 'SU'
-			END;
-		ELSE
-			BEGIN
-				SET @term_id_ce = @data_year + 'CE3'
-				SET @term_id_cu = @data_year + 'FA'
-			END;
-
-	PRINT @term_id_ce;
-	PRINT @term_id_cu;
-
- SELECT @STTRSTUDENT = [history].[STUDENT_TERMS].[STTR.STUDENT],
-		  @STTRTERM = [history].[STUDENT_TERMS].[STTR.STUDENT]
-	FROM [history].[STUDENT_TERMS]
-	WHERE [STTR.TERM] IN (@term_id_ce, @term_id_cu)
-    AND    [EffectiveDatetime] <= @report_date
-	AND   ([ExpirationDatetime] is null
-	OR	   [ExpirationDatetime] > @report_date)
-
-END;
-RETURN;
-END;
+ RETURN
+ SELECT DISTINCT [history].[STUDENT_TERMS].[STTR.STUDENT]
+   FROM [history].[STUDENT_TERMS]
+  WHERE [STTR.TERM] IN (
+        @data_year + CASE WHEN @data_term = '01' THEN 'CE1'
+                            WHEN @data_term = '02' THEN 'CE2'
+                            WHEN @data_term = '03' THEN 'CE3'
+                            ELSE 'XX' END, 
+        @data_year + CASE WHEN @data_term = '01' THEN 'SP'
+                            WHEN @data_term = '02' THEN 'SU'
+                            WHEN @data_term = '03' THEN 'FA'
+                            ELSE 'XX' END
+        )
+    --AND    [EffectiveDatetime] <= @report_date
+	--AND   ([ExpirationDatetime] is null
+	--OR	   [ExpirationDatetime] > @report_date)
 GO
 
 /*
-EXEC datamart.getSTUDENT_01 '2018', '01', '02/23/2018'
 
---, '2018SP'
+SELECT * FROM datamart.getSTUDENT_01('2018', '01', '02/23/2018')
+
+DECLARE @data_year varchar(4) = '2018',
+        @data_term varchar(2) = '01',
+        @report_date date = '02/23/2018'
+
+SELECT * FROM datamart.getSTUDENT_01(@data_year, @data_term, @report_date)
+
+-- */
