829a36b6f7da370d59e008ee07c9377316b4cb54
John W Cardwell <jwcardwell@haywood.edu>
2018-03-27 14:26:18 +0000

    Updated STUDENT_11.sql WIP

--- datamart/sql/STUDENT_11.sql
+++ datamart/sql/STUDENT_11.sql
@@ -1,8 +1,8 @@
-IF OBJECT_ID('datamart.getSTUDENT_07', 'P') IS NOT NULL
-	DROP PROCEDURE datamart.getSTUDENT_07
+IF OBJECT_ID('datamart.getSTUDENT_11', 'P') IS NOT NULL
+	DROP PROCEDURE datamart.getSTUDENT_11
 GO
 
-CREATE PROCEDURE datamart.getSTUDENT_07
+CREATE PROCEDURE datamart.getSTUDENT_11
 	@data_year varchar(4),
 	@data_term varchar(2),
 	@report_date date
@@ -37,10 +37,39 @@ WITH sal AS (
 		 , sal.[STA.ADMIT.STATUS]
 		 , sal.[STA.FED.COHORT.GROUP]
 		 , sal.[STA.START.TERM]
+		 , brows.[STA.TERMS]
+		 /*, sal.[STA.ATTEMPT.CREDS]
+		 , sal.[STA.CMPL.CRED]
+		 , sal.[STA.CUM.GPA]
+		 , sal.[X.STA.PROGRAM.GPA]*/
+		 , sal.[EffectiveDatetime]
+		 , sal.[ExpirationDatetime]
+	FROM [history].[STUDENT_ACAD_LEVELS] sal
+		LEFT JOIN history.STUDENT_ACAD_LEVELS_Current brows
+			ON (brows.[STA.ACAD.LEVEL] = sal.[STA.ACAD.LEVEL]
+				AND brows.[STA.STUDENT] = sal.[STA.STUDENT]
+				AND brows.[EffectiveDatetime] = sal.[EffectiveDatetime])
+	WHERE  sal.[EffectiveDatetime] <= @report_date
+	AND   (sal.[ExpirationDatetime] is null
+	OR	   sal.[ExpirationDatetime] > @report_date)
+) 
+	SELECT sal.[STA.STUDENT]
+		 , sal.[STA.ACAD.LEVEL]
+		 , sal.[STA.ADMIT.STATUS]
+		 , sal.[STA.FED.COHORT.GROUP]
+		 , sal.[STA.START.TERM]
 		 , sal.[STA.TERMS]
 		 /*, sal.[STA.ATTEMPT.CREDS]
 		 , sal.[STA.CMPL.CRED]
 		 , sal.[STA.CUM.GPA]
 		 , sal.[X.STA.PROGRAM.GPA]*/
-	FROM [history].[STUDENT_ACAD_LEVELS] sal
-), 
+	FROM sal
+	/*INNER JOIN datamart.getSTUDENT_10(@data_year, @data_term, @report_date) s10 
+         ON ()*/
+;
+
+/*
+EXEC datamart.getSTUDENT_11 '2018', '01', '02/23/2018'
+
+--, '2018SP'
+*/
